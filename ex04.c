ee6f37e91bddc11749f405abc5209d22b73ad0ec
Jennerson <jennerson.fb@gmail.com>
2019-08-24 23:51:10 -0300

    Att

--- ex04.c
+++ ex04.c
@@ -3,10 +3,12 @@
 
 int main(){
 	
-	int i, soma;
-	
-	for(i=1;i<=64;i++){
-		soma = soma + (i*2);
+	int i, soma=1, ant=0, graos=1;
+
+	for(i=0;i<64;i++){
+		ant = graos * 2;
+		graos = graos * 2;
+		soma = soma + graos;
 	}
 	printf("Soma= %d", soma);
 }

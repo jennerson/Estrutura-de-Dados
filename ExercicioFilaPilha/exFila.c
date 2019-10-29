e57d8e5af38a9da6438b1307e9fe22104f42157c
Jennerson <jennerson.fb@gmail.com>
2019-10-28 23:33:23 -0300

    Exercício 2 Estágio
    Implementação Fila e Pilha

---
+++ ExercicioFilaPilha/exFila.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+//criaFila
+	
+//inserir
+	
+//remover
+	
+//exibir
+	
+//esvaziar
+
+//isVazia
+
+int main(){
+
+	//Exercício de Fila, slides 27 e 28.
+	
+	return 0;
+}
---
+++ ExercicioFilaPilha/exPilha.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+//criaPilha
+	
+//isVazia
+	
+//isCheia
+	
+//getUltimo
+	
+//push
+	
+//pop
+	
+//exibir
+
+int main(){
+
+	// Exercício de Pilha, slide 19
+	
+	return 0;
+}
